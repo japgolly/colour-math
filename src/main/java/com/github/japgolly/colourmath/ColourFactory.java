874bc108c8954b7b6695a06b40b78355807703c3
David Barri <japgolly@gmail.com>
2013-01-13 14:19:06 +1100

    Reordered factory methods.

--- src/main/java/com/github/japgolly/colourmath/ColourFactory.java
+++ src/main/java/com/github/japgolly/colourmath/ColourFactory.java
@@ -19,36 +19,18 @@ public class ColourFactory {
 		return INSTANCE;
 	}
 
-	public ColourHSL01 HSL01(float h, float s, float l) {
-		return new ColourHSL01(h, s, l);
+	// ------------------------------------------------------------------------------------
+
+	public ColourRGB255 RGB(int rgb) {
+		return new ColourRGB255((rgb & 0xFF0000) >>> 16, (rgb & 0xFF00) >>> 8, rgb & 0xFF);
 	}
 
-	public ColourHSL01 HSL01(float[] hsl) {
-		return new ColourHSL01(hsl[0], hsl[1], hsl[2]);
+	public ColourRGB255 ARGB(int argb) {
+		return RGB(argb);
 	}
 
-	public ColourHSL255 HSL255(int h, int s, int l) {
-		return new ColourHSL255(h, s, l);
-	}
-
-	public ColourHSL255 HSL255(int[] hsl) {
-		return new ColourHSL255(hsl[0], hsl[1], hsl[2]);
-	}
-
-	public ColourLab Lab(double l, double a, double b) {
-		return new ColourLab(l, a, b, Illuminants.getDefault());
-	}
-
-	public ColourLab Lab(double[] lab) {
-		return new ColourLab(lab[0], lab[1], lab[2], Illuminants.getDefault());
-	}
-
-	public ColourLab Lab(double l, double a, double b, Illuminant illuminant) {
-		return new ColourLab(l, a, b, illuminant);
-	}
-
-	public ColourLab Lab(double[] lab, Illuminant illuminant) {
-		return new ColourLab(lab[0], lab[1], lab[2], illuminant);
+	public ColourRGB255 RGBA(int rgba) {
+		return new ColourRGB255((rgba & 0xFF000000) >>> 24, (rgba & 0xFF0000) >>> 16, (rgba & 0xFF00) >>> 8);
 	}
 
 	public ColourRGB01 RGB01(float r, float g, float b) {
@@ -67,17 +49,25 @@ public class ColourFactory {
 		return new ColourRGB255(rgb[0], rgb[1], rgb[2]);
 	}
 
-	public ColourRGB255 RGB(int rgb) {
-		return new ColourRGB255((rgb & 0xFF0000) >>> 16, (rgb & 0xFF00) >>> 8, rgb & 0xFF);
+	// ------------------------------------------------------------------------------------
+
+	public ColourHSL01 HSL01(float h, float s, float l) {
+		return new ColourHSL01(h, s, l);
 	}
 
-	public ColourRGB255 ARGB(int argb) {
-		return RGB(argb);
+	public ColourHSL01 HSL01(float[] hsl) {
+		return new ColourHSL01(hsl[0], hsl[1], hsl[2]);
 	}
 
-	public ColourRGB255 RGBA(int rgba) {
-		return new ColourRGB255((rgba & 0xFF000000) >>> 24, (rgba & 0xFF0000) >>> 16, (rgba & 0xFF00) >>> 8);
+	public ColourHSL255 HSL255(int h, int s, int l) {
+		return new ColourHSL255(h, s, l);
 	}
+
+	public ColourHSL255 HSL255(int[] hsl) {
+		return new ColourHSL255(hsl[0], hsl[1], hsl[2]);
+	}
+
+	// ------------------------------------------------------------------------------------
 
 	public ColourXYZ XYZ(double x, double y, double z) {
 		return new ColourXYZ(x, y, z, Illuminants.getDefault());
@@ -93,5 +83,23 @@ public class ColourFactory {
 
 	public ColourXYZ XYZ(double[] xyz, Illuminant illuminant) {
 		return new ColourXYZ(xyz[0], xyz[1], xyz[2], illuminant);
+	}
+
+	// ------------------------------------------------------------------------------------
+
+	public ColourLab Lab(double l, double a, double b) {
+		return new ColourLab(l, a, b, Illuminants.getDefault());
+	}
+
+	public ColourLab Lab(double l, double a, double b, Illuminant illuminant) {
+		return new ColourLab(l, a, b, illuminant);
+	}
+
+	public ColourLab Lab(double[] lab) {
+		return new ColourLab(lab[0], lab[1], lab[2], Illuminants.getDefault());
+	}
+
+	public ColourLab Lab(double[] lab, Illuminant illuminant) {
+		return new ColourLab(lab[0], lab[1], lab[2], illuminant);
 	}
 }
